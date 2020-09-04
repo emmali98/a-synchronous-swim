8770904c9d43339ce89cbe032f08e552e274db0a
Emma Li <emma.m.li.98@gmail.com>
2020-09-04 16:34:45 -0700

    Changes GET response on server side to terminal keypresses
    TODO: Change httpHandlerSpec test, change client-side GET request interval.

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -2,24 +2,23 @@ const fs = require('fs');
 const path = require('path');
 const headers = require('./cors');
 const multipart = require('./multipartUtils');
+const messageQueue = require('./messageQueue');
 
 // Path for the background image ///////////////////////
 module.exports.backgroundImageFile = path.join('.', 'background.jpg');
 ////////////////////////////////////////////////////////
 
-let messageQueue = null;
-module.exports.initialize = (queue) => {
-  messageQueue = queue;
-};
+// let messageQueue = null;
+// module.exports.initialize = (queue) => {
+//   messageQueue = queue;
+// };
 
 module.exports.router = (req, res, next = ()=>{}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
   res.writeHead(200, headers);
   if (req.method === 'GET') {
-    var validMessages = ['left', 'right', 'up', 'down'];
-    var messageIdx = Math.floor(Math.random() * 4);
-    var message = validMessages[messageIdx];
-    res.write(message);
+    var message = messageQueue.dequeue();
+    res.write(message ? message : '');
   }
   res.end();
   next(); // invoke next() at the end of a request to help with testing!
